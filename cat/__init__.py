af40aad8ae50acbe54ee798f7a0ee027ebdef08f
Kris Su <cysu1205@gmail.com>
2019-10-27 18:23:23 -0400

    Updated frontend, added errors handlers

--- cat/__init__.py
+++ cat/__init__.py
@@ -23,17 +23,20 @@ def create_app(config_class=Config):
 	db.init_app(app)
 	admin.init_app(app)
 	from cat.models import User, Trivia
-	admin.add_view(ModelView(models.User, db.session))
-	admin.add_view(ModelView(models.Trivia, db.session))
 	bcrypt.init_app(app)
 	login_manager.init_app(app)
 	mail.init_app(app)
 
+	admin.add_view(ModelView(models.User, db.session))
+	admin.add_view(ModelView(models.Trivia, db.session))
+
 	from cat.main.routes import main
 	from cat.users.routes import users
 	from cat.trivia.routes import triviaa
+	from cat.errors.handlers import errors
 	app.register_blueprint(main)
 	app.register_blueprint(users)
 	app.register_blueprint(triviaa)
+	app.register_blueprint(errors)
 
 	return app
