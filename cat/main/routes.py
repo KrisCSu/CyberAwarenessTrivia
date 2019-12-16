7e63e92473548e063643169bf6826de786268d23
Kris Su <kris@Kriss-MacBook-Pro.local>
2019-12-16 17:55:48 -0500

    fixed about page

--- cat/main/routes.py
+++ cat/main/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, request, Blueprint
+from flask import render_template, request, redirect, url_for, Blueprint
+from flask_login import login_required
 from cat.models import Result
 from sqlalchemy import desc
 
@@ -7,6 +8,7 @@ main = Blueprint('main', __name__)
 
 @main.route("/")
 @main.route("/index")
+@login_required
 def index():
 		results = Result.query.order_by(desc(Result.date)).limit(10).all()
 		return render_template('index.html', results=results)
